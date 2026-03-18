fafba1c41faedc29a9d85903ea2a75f3edbd2a9f
Fisherqwq <143472947+Fisherqwq@users.noreply.github.com>
2026-03-18 12:51:03 +0800

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int game[9][9];
-int done(int x,int y);
+int done();
 int change(int x,int y,int val,int to);
 int xused[9][10]={0};
 int yused[9][10]={0};
@@ -16,7 +16,7 @@ int main() {
       }
     }
   }
-  if(done(0,0)){
+  if(done()){
     for(int i=0;i<9;i++){
       for(int j=0;j<9;j++){
         printf("%d ",game[i][j]);
@@ -26,30 +26,34 @@ int main() {
   }
   return 0;
 }
-int done(int x,int y){
-  if(x!=9){
-    if(game[x][y]==0){
-      for(int u=1;u<=9;u++){
-        if(xused[x][u]==0&&yused[y][u]==0&&boxused[(x/3)*3+y/3][u]==0){
-          game[x][y]=u; 
-          change(x,y,u,1);
-          if(y<8){
-            if (done(x,y+1)) return 1;
+int done(){
+  int mp=10,bx=-1,by=-1;
+  for(int i=0;i<9;i++){
+    for(int j=0;j<9;j++){
+      if(game[i][j]==0){
+        int p=0;
+        for(int u=1;u<=9;u++){
+          if(!xused[i][u]&&!yused[j][u]&&!boxused[(i/3)*3+j/3][u]){
+            p++;
           }
-          else{
-            if (done(x+1,0)) return 1;
-          }
-          change(x,y,u,0);
-          game[x][y]=0;
+        }
+        if(p==0) return 0;
+        if(p<mp){
+          mp=p;
+          bx=i;
+          by=j;
         }
       }
     }
-    else{
-      if(y<8){
-        if (done(x,y+1)) return 1;
-      }
-      else{
-        if (done(x+1,0)) return 1;
+  }
+  if(bx!=-1){
+    for(int u=1;u<=9;u++){
+      if(xused[bx][u]==0&&yused[by][u]==0&&boxused[(bx/3)*3+by/3][u]==0){
+        game[bx][by]=u; 
+        change(bx,by,u,1);
+        if (done()) return 1;
+        change(bx,by,u,0);
+        game[bx][by]=0;
       }
     }
   }
